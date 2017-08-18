601abadfc460f32ff49f9f92aa0fddb8c7749665
rongself <rongself@qq.com>
2017-08-18 15:44:33 +0800

    improve config

--- config/sms.php
+++ config/sms.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
-    'api_key' => '',
-    'use_ssl' => false,
+    'api_key' => env('LUOSIMAO_API_KEY',''),
+    'use_ssl' => env('LUOSIMAO_USE_SSL',false),
+    'sign' => env('LUOSIMAO_SIGN','铁壳测试'),
 ];
